94822786bf05453a891f669e3a544dbb03932c0f
Katz <katzyoav10@gmail.com>
2020-10-04 16:14:48 +0300

    .

--- strategy.py
+++ strategy.py
@@ -1,7 +1,20 @@
 
 class BaseStrategy:
 
+	def __init__(self, envs):
+		self.envelopes = envs
+
 	def play(self):
+		self.perform_strategy()
+
+	def perform_strategy(self):
+		pass
 
 
-	def perform_strategy(self, counter):
+class More_then_N_percent_group_strategy:
+
+	def __init__(self, envs, p):
+		super(self, envs)
+		self.percent = p
+
+	def perform_strategy(self):
