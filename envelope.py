007cdfe9931caf03b4530f7645c1175509777ec9
yoka255 <71012647+yoka255@users.noreply.github.com>
2020-10-06 16:33:41 +0300

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -1,5 +1,9 @@
 import random
 class Envelope:
+  """
+    Class to represent an envelope using the fields money (which represents the amount of money in the envelope)
+    and used (boolean which represents whether the envelope was used or not)
+  """
   def __init__(self, money=0, used=False):
     self.money = random.randint(0, 1000)
     self.used = used
