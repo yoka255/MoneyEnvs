e584aae35f4862f73721e8acf92f546f8ed7dc59
yoka255 <71012647+yoka255@users.noreply.github.com>
2020-10-06 16:16:31 +0300

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -1,5 +1,5 @@
 import random
 class Envelope:
-  def __init__(self, money=random.randint(0, 1000), used=False):
-    self.money = money
+  def __init__(self, money=0, used=False):
+    self.money = random.randint(0, 1000)
     self.used = used
