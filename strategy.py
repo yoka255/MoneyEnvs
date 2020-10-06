a73a7c67c1e0b802fdcf7ae496a33abba02fdc9b
saharshaul <65076361+saharshaul@users.noreply.github.com>
2020-10-06 16:31:45 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -9,15 +9,15 @@ class BaseStrategy:
 
     def perform_strategy(self):
         for envelope in self.envelopes:
-            letter = input("The amount is: " + str(envelope.money) + " Take(T) or Leave(L)")
+            letter = input("The amount is: " + str(envelope.money) + " Take(T) or Leave(L) - ")
             if letter == 'T':
                 print(envelope.money)
                 return
             elif letter == 'L':
                 pass
             else:
-                print("Wrong Input Envelope Was Skipped")
-        print("returning last envelope")
+                print("Wrong Input - Envelope Was Skipped")
+        print("Returning last envelope")
         print(self.envelopes[-1].money)
 
     def display(self):
@@ -25,21 +25,24 @@ class BaseStrategy:
 
 
 class Automatic_BaseStrategy(BaseStrategy):
+    def __init__(self, envs):
+        super().__init__(envs)
 
-    def perform_startegy(self):
+    def perform_strategy(self):
         rnd = random.randint(0, 99)
-        print(self.envelopes[rnd].money)
+        print("The amount is: " + str(self.envelopes[rnd].money))
 
     def display(self):
         return "Automatic_BaseStrategy - random selection of envelop"
 
 
 class More_then_N_percent_group_strategy(BaseStrategy):
-
-    def __init__(self, envs, p):
+    def __init__(self, envs, p=0.25):
+        super().__init__(envs)
         self.percent = p
 
     def perform_strategy(self):
+        self.percent = float(self.percent)
         n = len(self.envelopes)
         best = 0
         for e in self.envelopes[:int(n * self.percent)]:
@@ -56,9 +59,10 @@ class More_then_N_percent_group_strategy(BaseStrategy):
 
 class N_max_strategy(BaseStrategy):
     def __init__(self, envs, n=3):
+        super().__init__(envs)
         self.N = n
 
-    def perform_startegy(self):
+    def perform_strategy(self):
         count = 1
         max = 0
         i = 0
@@ -67,7 +71,7 @@ class N_max_strategy(BaseStrategy):
                 max = self.envelopes[i].money
                 count += 1
             i += 1
-        print(self.envelopes[i].money)
+        print("The amount is: " + str(self.envelopes[i].money))
 
     def display(self):
         return "N_max_strategy - return envelope after N max values (defualt N=3)"
