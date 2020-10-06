2d23df6d2ff575a995197c284e246c1d93d6b530
Katz <katzyoav10@gmail.com>
2020-10-06 15:25:28 +0300

    .

--- strategy.py
+++ strategy.py
@@ -11,6 +11,35 @@ class BaseStrategy(ABC):
 		pass
 
 
+class Automatic_BaseStrategy(BaseStrategy):
+    def __init__(self, envs):
+        super(self, envs)
+
+    def perform_startegy(self, counter):
+        rnd = random.randint(0, 99)
+        return self.envelopes[rnd]
+
+
+class Automatic_BaseStrategy(BaseStrategy):
+    def __init__(self, n):
+        super(self,n)
+
+    def perform_strategy(self):
+        while envelope in self.EnvelopeArr:
+            print(envelope.money)
+            a = input("The amount is:" + envelope.money + " Take(T) or Leave(L)")
+            if(a == 'T'):
+                return envelope
+
+            elif(a=='L'):
+                pass
+                temp = envelope
+            else:
+                print("Wrong Input Envelope Was Skipped")
+        print("returning last envelope")
+        return temp
+
+
 class More_then_N_percent_group_strategy:
 
 	def __init__(self, envs, p):
@@ -26,3 +55,20 @@ class More_then_N_percent_group_strategy:
 				print(f"The envelope that was chosen contains {e.money}$")
 				return
 		print(f"The envelope that was chosen contains {envelopes[-1].money}$")
+
+
+class N_max_srategy(BaseStrategy):
+    def __init__(self, envs, n):
+        super(self, envs)
+        self.n = n
+
+    def perform_startegy(self, counter):
+        count = 1
+        max = 0
+        i = 0
+        while count < self.n:
+            if self.envelopes[i] > max:
+                max = self.envelopes[i]
+                count += 1
+            i += 1
+        return self.envelopes[i]
