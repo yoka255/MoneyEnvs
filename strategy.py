8acf8e7423fd73e32e5a8dfc7c32f8491fc610b2
yoka255 <71012647+yoka255@users.noreply.github.com>
2020-10-06 16:50:21 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,47 +1,71 @@
+"""
+    Authors: Yoav Katz, Guy Haviv, Sahar Shaul.
+    Date: 06/10/2020
+"""
+
 import random
 
 class BaseStrategy:
+	"""
+		Class which represents the basic strategy of letting the user select envelopes manually.
+		Also serves as the base class for other strategy classes.
+	"""
+
     def __init__(self, envs):
+    	# Constructor function
         self.envelopes = envs
 
+
     def play(self):
+    	# Function which activates the strategy on the envelopes which were defined before.
         self.perform_strategy()
 
+
     def perform_strategy(self):
+    	# Lets the user manually choose for each envelope whether he likes to take it or not.
         for envelope in self.envelopes:
             letter = input("The amount is: " + str(envelope.money) + " Take(T) or Leave(L) - ")
             if letter == 'T':
                 print(envelope.money)
                 return
-            elif letter == 'L':
-                pass
-            else:
+            elif letter != 'L':
                 print("Wrong Input - Envelope Was Skipped")
         print("Returning last envelope")
         print(self.envelopes[-1].money)
 
+
     def display(self):
-        return "BaseStrategy - user select manually envelopes"
+    	# Returns a message which displays what the class does
+        return r"BaseStrategy - user select manually envelopes"
 
 
 class Automatic_BaseStrategy(BaseStrategy):
+
     def __init__(self, envs):
+    	# Constructor function
         super().__init__(envs)
 
+
     def perform_strategy(self):
-        rnd = random.randint(0, 99)
+    	#Chooses a random envelope
+        rnd = random.randint(0, len(envelopes)-1)
         print("The amount is: " + str(self.envelopes[rnd].money))
 
     def display(self):
-        return "Automatic_BaseStrategy - random selection of envelop"
+    	# Returns a message which displays what the class does
+        return r"Automatic_BaseStrategy - random selection of envelop"
 
 
 class More_then_N_percent_group_strategy(BaseStrategy):
+
     def __init__(self, envs, p=0.25):
+    	# Constructor function
         super().__init__(envs)
         self.percent = p
 
+
     def perform_strategy(self):
+    	# Choose envelope with more money that in the highest of N% group
         self.percent = float(self.percent)
         n = len(self.envelopes)
         best = 0
@@ -53,16 +77,20 @@ class More_then_N_percent_group_strategy(BaseStrategy):
                 return
         print(f"The envelope that was chosen contains {self.envelopes[-1].money}$")
 
+
     def display(self):
-        return "More_then_N_percent_group_strategy - return envelope with more money that in the highest of N% group"
+    	# Returns a message which displays what the class does
+        return r"More_then_N_percent_group_strategy - return envelope with more money that in the highest of N% group"
 
 
 class N_max_strategy(BaseStrategy):
     def __init__(self, envs, n=3):
-        super().__init__(envs)
+    	# Constructor function
+    	super().__init__(envs)
         self.N = n
 
     def perform_strategy(self):
+    	# Choose envelope after N max values (defualt N=3)
         count = 1
         max = 0
         i = 0
@@ -74,4 +102,5 @@ class N_max_strategy(BaseStrategy):
         print("The amount is: " + str(self.envelopes[i].money))
 
     def display(self):
-        return "N_max_strategy - return envelope after N max values (defualt N=3)"
+    	# Returns a message which displays what the class does
+        return r"N_max_strategy - return envelope after N max values (defualt N=3)"
